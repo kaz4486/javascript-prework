91b09db89730e62ec40e99dc5d2d27d721032244
Kamil <kaz4486@gmail.com>
2022-03-04 15:37:49 +0100

    Add if...else for results

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
@@ -30,8 +31,38 @@ else if(playerInput == '3'){
     playerMove = 'nożyce';
 }
 
+
 printMessage('Twój ruch to: ' + playerMove);
 
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
 
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+}
 
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz!');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+}
+
+if( computerMove == playerMove){
+    printMessage('Remis!');
+}
+
+if( playerMove == 'nieznany ruch'){
+    printMessage('Musisz wybrać opcję 1,2 lub 3!');
+}
