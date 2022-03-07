960b6d5f0c3feddc0eec194b3b53bacf1dec66ad
Kamil <kaz4486@gmail.com>
2022-03-07 11:20:02 +0100

    Change logic for result

--- js/script.js
+++ js/script.js
@@ -35,34 +35,20 @@ else if(playerInput == '3'){
 printMessage('Twój ruch to: ' + playerMove);
 
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
+if( computerMove == 'kamień' && playerMove == 'papier') 
+    ( computerMove == 'papier' && playerMove == 'nożyce')
+    ( computerMove == 'nożyce' && playerMove == 'kamień'){
     printMessage('Ty wygrywasz!');
 }
 
-else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Przegrywasz!');
-}
-
-if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-}
-
-else if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Przegrywasz!');
-}
-
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-}
-
-else if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Przegrywasz!');
-}
-
-if( computerMove == playerMove){
+else if( computerMove == playerMove){
     printMessage('Remis!');
 }
 
-if( playerMove == 'nieznany ruch'){
+else if( playerMove == 'nieznany ruch'){
     printMessage('Musisz wybrać opcję 1,2 lub 3!');
 }
+else {
+    printMessage('Przegrywasz!');
+}
+
